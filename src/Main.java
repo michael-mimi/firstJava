ac361f1c55487d3b9ef094b924a15062cee66bdf
michael-mimi <michenfei36@hotmail.com>
2024-04-08 15:21:00 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileWriter;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 
@@ -59,21 +65,51 @@ public class Main {
 
         // MyFrame frame = new MyFrame();
 
-        File file = new File("src/secret_message.txt");
-        if (file.exists()) {
-            System.out.println("That file exists! :O!");
-            System.out.println(file.getAbsolutePath());
-        } else {
-            System.out.println("That file doesn't exist :(");
-        }
-        try {
-            FileWriter writer = new FileWriter(file);
-            writer.write("Now it's the new beginning!Horray!");
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // File file = new File("src/secret_message.txt");
+        // if (file.exists()) {
+        //     System.out.println("That file exists! :O!");
+        //     System.out.println(file.getAbsolutePath());
+        // } else {
+        //     System.out.println("That file doesn't exist :(");
+        // }
+        // try {
+        //     FileWriter writer = new FileWriter(file);
+        //     writer.write("Now it's the new beginning!Horray!");
+        //     writer.close();
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new BorderLayout(0,10));
+        frame.setVisible(true);
         
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.yellow);
+        panel4.setBackground(Color.magenta);
+        panel5.setBackground(Color.blue);
+
+        panel1.setPreferredSize(new Dimension(100,100));
+        panel2.setPreferredSize(new Dimension(100,100));
+        panel3.setPreferredSize(new Dimension(100,100));
+        panel4.setPreferredSize(new Dimension(100,100));
+        panel5.setPreferredSize(new Dimension(100,100));
+
+        frame.add(panel1, BorderLayout.NORTH);
+        frame.add(panel2, BorderLayout.WEST);
+        frame.add(panel3, BorderLayout.CENTER);
+        frame.add(panel4, BorderLayout.SOUTH);
+        frame.add(panel5, BorderLayout.EAST);
 
     }
 
